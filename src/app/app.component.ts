4ee34d6a2feb45c78463b6bf1be862a4f8605613
Seth Condie <sethhaskellcondie@gmail.com>
2026-01-21 22:02:16 -0700

    Implemented the table size settings.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { PlayerComponent } from './player/player.component';
 
 @Component({
   selector: 'app-root',
-  imports: [PlayerComponent],
+  imports: [CommonModule, PlayerComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
@@ -17,6 +18,7 @@ export class AppComponent {
   minions = 1;
   demons = 1;
   showSettings = true;
+  tableSize: 'small' | 'medium' | 'large' = 'small';
 
   hideSettings(): void {
     this.showSettings = false;
@@ -24,5 +26,21 @@ export class AppComponent {
 
   displaySettings(): void {
     this.showSettings = true;
+  }
+
+  tableSizeUp(): void {
+    if (this.tableSize === 'small') {
+      this.tableSize = 'medium';
+    } else if (this.tableSize === 'medium') {
+      this.tableSize = 'large';
+    }
+  }
+
+  tableSizeDown(): void {
+    if (this.tableSize === 'large') {
+      this.tableSize = 'medium';
+    } else if (this.tableSize === 'medium') {
+      this.tableSize = 'small';
+    }
   }
 }
