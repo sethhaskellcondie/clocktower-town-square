f8b02073422429940de8d220a2488a783cc5421d
Seth Condie <sethhaskellcondie@gmail.com>
2026-01-21 21:40:28 -0700

    Included the tables for the generic information for the game.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,4 +9,20 @@ import { PlayerComponent } from './player/player.component';
 })
 export class AppComponent {
   title = 'clocktower-town-square';
+  playerCount = 1;
+  alive = 1;
+  ghostVotes = 1;
+  townsfolk = 1;
+  outsiders = 1;
+  minions = 1;
+  demons = 1;
+  showSettings = true;
+
+  hideSettings(): void {
+    this.showSettings = false;
+  }
+
+  displaySettings(): void {
+    this.showSettings = true;
+  }
 }
